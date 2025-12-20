98e8d855cc28d4336db79fc7a70a17f5afa14871
AxmedovCA <rufatba-mpa201@code.edu.az>
2025-12-20 17:34:05 +0400

    first commit

---
+++ Models/Shipping.cs
@@ -0,0 +1,12 @@
+﻿namespace Pronia.Models
+{
+    public class Shipping
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; } 
+
+        public string ImageUrl { get; set; }
+    }
+}
