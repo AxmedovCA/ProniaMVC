2d394c8edbbd42440dd7878365bfdc3ee0265c2b
AxmedovCA <rufatba-mpa201@code.edu.az>
2025-12-23 03:20:53 +0400

    Added bug :)

--- Models/Shipping.cs
+++ Models/Shipping.cs
@@ -1,12 +1,16 @@
-﻿namespace Pronia.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pronia.Models
 {
     public class Shipping
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
-
-        public string Description { get; set; } 
-
+        [MaxLength (256)]
+        public string Description { get; set; }
+        [MaxLength(512),MinLength(3)]
         public string ImageUrl { get; set; }
     }
 }
--- Models/Slider.cs
+++ Models/Slider.cs
@@ -1,11 +1,17 @@
-﻿namespace Pronia.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pronia.Models
 {
     public class Slider
     {
         public int Id { get; set; }
+        [MaxLength(100)]
         public string Title { get; set; }
+        [MaxLength(256)]
         public string Description { get; set; }
-        public string ImageUrl { get; set; }
+        [MaxLength (512),MinLength(3)]
+		[Required]
+		public string ImageUrl { get; set; }
         public decimal DiscountPercentage { get; set; }
     }
 
