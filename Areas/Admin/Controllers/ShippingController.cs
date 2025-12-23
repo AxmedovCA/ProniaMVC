f8ce2b817df30c1081cec5c2f2b0ca9ca63ba947
AxmedovCA <rufatba-mpa201@code.edu.az>
2025-12-23 22:23:04 +0400

    AddedUpdate

--- Areas/Admin/Controllers/ShippingController.cs
+++ Areas/Admin/Controllers/ShippingController.cs
@@ -21,6 +21,7 @@ namespace Pronia.Areas.Admin.Controllers
 		}
 
 		[HttpPost]
+		[AutoValidateAntiforgeryToken]
 		public IActionResult CreateShipping(Shipping shipping) {
 			if (ModelState.IsValid == false)
 			{
@@ -42,6 +43,32 @@ namespace Pronia.Areas.Admin.Controllers
 			_context.SaveChanges();
 			return RedirectToAction("Index");
 		}
+		[HttpGet]
+		public IActionResult Update(int id)
+		{
+			var shipping = _context.Shippings.Find(id);
+			if(shipping is null)
+			{
+				return NotFound();
+			}
+			return View(shipping);
+		}
+		[HttpPost]
+		[AutoValidateAntiforgeryToken]
+		public IActionResult Update(Shipping shipping)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View();
+			}
+			var isExist = _context.Shippings.Find(shipping.Id);
+			isExist.Title = shipping.Title;
+			isExist.Description = shipping.Description;
+			isExist.ImageUrl = shipping.ImageUrl;
 
+			_context.Shippings.Update(isExist);
+			_context.SaveChanges();
+			return RedirectToAction(nameof(Index));
+		}
 	}
 }
--- Areas/Admin/Controllers/SliderController.cs
+++ Areas/Admin/Controllers/SliderController.cs
@@ -7,7 +7,7 @@ namespace Pronia.Areas.Admin.Controllers
     [Area("Admin")]
     public class SliderController(AppDbContext _context) : Controller
     {
-       
+
         public IActionResult Index()
         {
             var sliders = _context.Sliders.ToList();
@@ -19,32 +19,65 @@ namespace Pronia.Areas.Admin.Controllers
             return View();
         }
         [HttpPost]
+        [AutoValidateAntiforgeryToken]
         public IActionResult Create(Slider slider)
         {
-            if(ModelState.IsValid ==false)
+            if (ModelState.IsValid == false)
             {
                 return View();
             }
 
-            if(slider.DiscountPercentage <0 ||  slider.DiscountPercentage > 100)
+            if (slider.DiscountPercentage < 0 || slider.DiscountPercentage > 100)
             {
                 ModelState.AddModelError("DiscountPercentage", "Endirim 0 100 araliqinda olmalidir");
                 return View();
             }
             _context.Sliders.Add(slider);
-            _context.SaveChanges(); 
+            _context.SaveChanges();
             return RedirectToAction("Index");
         }
         public IActionResult Delete(int id)
         {
             var slider = _context.Sliders.Find(id);
-            if(slider == null) {
-            return NotFound();
+            if (slider == null)
+            {
+                return NotFound();
             }
             _context.Sliders.Remove(slider);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
-        
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var slider = _context.Sliders.Find(id);
+            if (slider is null)
+            {
+                return NotFound();
+            }
+            return View(slider);
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Update(Slider slider)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var existSlider = _context.Sliders.Find(slider.Id);
+            if (existSlider is null)
+            {
+                return NotFound();
+            }
+            existSlider.Title= slider.Title;
+            existSlider.Description= slider.Description;
+            existSlider.ImageUrl= slider.ImageUrl;
+            existSlider.DiscountPercentage = slider.DiscountPercentage; 
+            _context.Sliders.Update(existSlider);
+            _context.SaveChanges(); 
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
