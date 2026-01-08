c492f6f934cdab516e8628347956e3bfd13ecebc
AxmedovCA <rufatba-mpa201@code.edu.az>
2026-01-08 11:40:08 +0400

    Email Sevices

---
+++ Abstraction/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿namespace Pronia.Abstraction
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string email,string subject,string body);
+
+
+    }
+}
--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Pronia.Abstraction;
 using Pronia.Contexts;
 using Pronia.ViewModels.UserViewModels;
 
 namespace Pronia.Controllers
 {
-    public class AccountController(UserManager<AppUser> _userManager,SignInManager<AppUser> _signInManager, RoleManager<IdentityRole> _roleManager,IConfiguration configuration) : Controller
+    public class AccountController(UserManager<AppUser> _userManager, SignInManager<AppUser> _signInManager, RoleManager<IdentityRole> _roleManager, IConfiguration configuration, IEmailService _emailService) : Controller
     {
         public IActionResult Register()
         {
@@ -16,12 +17,13 @@ namespace Pronia.Controllers
         [HttpPost]
         public async Task<IActionResult> Register(RegisterVM vm)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return View(vm);
             }
             var existUser = await _userManager.FindByNameAsync(vm.UserName);
-            if (existUser is { }) {
+            if (existUser is { })
+            {
                 ModelState.AddModelError("UserName", "This username is already exist");
                 return View(vm);
             }
@@ -40,17 +42,22 @@ namespace Pronia.Controllers
 
             };
             var result = await _userManager.CreateAsync(user, vm.Password);
-            if(result.Succeeded==false)
+            if (result.Succeeded == false)
             {
-                foreach(var error in  result.Errors)
+                foreach (var error in result.Errors)
                 {
                     ModelState.AddModelError("", error.Description);
                 }
                 return View(vm);
             }
-            await _signInManager.SignInAsync(user, isPersistent: false);
+
+
             await _userManager.AddToRoleAsync(user, "Member");
-            return RedirectToAction("Index","Home");
+            await SendConfirmationEmail(user);
+
+            TempData["SuccessMessage"] = "Please confirm your email";
+
+            return RedirectToAction("Login");
         }
         public IActionResult Login()
         {
@@ -63,36 +70,45 @@ namespace Pronia.Controllers
             {
                 return View(vm);
             }
-                var user = await _userManager.FindByEmailAsync(vm.EmailAddress);
-                if(user is null)
-                {
-                    ModelState.AddModelError("", "Email or password is wrong");
-                    return View(vm);
-                }
-                 var result = await  _userManager.CheckPasswordAsync(user, vm.Password);
-                if (result == false)
-                {
-                    ModelState.AddModelError("", "Email or password is wrong");
-                    return View(vm);
-                }
+            var user = await _userManager.FindByEmailAsync(vm.EmailAddress);
+            if (user is null)
+            {
+                ModelState.AddModelError("", "Email or password is wrong");
+                return View(vm);
+            }
+            var result = await _userManager.CheckPasswordAsync(user, vm.Password);
+            if (result == false)
+            {
+                ModelState.AddModelError("", "Email or password is wrong");
+                return View(vm);
+            }
+            if (!user.EmailConfirmed)
+            {
+                ModelState.AddModelError("", "Please confirm your email");
+                await SendConfirmationEmail(user);
+                return View(vm);
+            }
             await _signInManager.SignInAsync(user, vm.IsRemember);
             return RedirectToAction("Index", "Home");
 
         }
-        public  async Task<IActionResult> Logout()
+        public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
             return RedirectToAction(nameof(Login));
         }
         public async Task<IActionResult> CreateRoles()
         {
-            await _roleManager.CreateAsync(new IdentityRole(){
+            await _roleManager.CreateAsync(new IdentityRole()
+            {
                 Name = "moderator"
-            }); 
-            await _roleManager.CreateAsync(new IdentityRole(){
+            });
+            await _roleManager.CreateAsync(new IdentityRole()
+            {
                 Name = "Member"
-            }); 
-            await _roleManager.CreateAsync(new IdentityRole(){
+            });
+            await _roleManager.CreateAsync(new IdentityRole()
+            {
                 Name = "Moderator"
             });
             return Ok("Roles created");
@@ -102,32 +118,68 @@ namespace Pronia.Controllers
 
             var adminUserVM = configuration.GetSection("AdminUser").Get<UserVM>();
             var moderatorUserVM = configuration.GetSection("ModeratorUser").Get<UserVM>();
-            if(adminUserVM is not null) { 
-            AppUser adminUser = new()
+            if (adminUserVM is not null)
             {
-                FirstName = adminUserVM.FirstName,
-                LastName = adminUserVM.LastName,
-                Email = adminUserVM.Email,
-                UserName = adminUserVM.UserName,
-            };
+                AppUser adminUser = new()
+                {
+                    FirstName = adminUserVM.FirstName,
+                    LastName = adminUserVM.LastName,
+                    Email = adminUserVM.Email,
+                    UserName = adminUserVM.UserName,
+                };
 
-            await _userManager.CreateAsync(adminUser, adminUserVM.Password);
-            await _userManager.AddToRoleAsync(adminUser, "Admin");
+                await _userManager.CreateAsync(adminUser, adminUserVM.Password);
+                await _userManager.AddToRoleAsync(adminUser, "Admin");
             }
-            if(moderatorUserVM is not null) { 
-            AppUser moderatorUser = new()
+            if (moderatorUserVM is not null)
             {
-                FirstName = moderatorUserVM.FirstName,
-                LastName = moderatorUserVM.LastName,
-                Email = moderatorUserVM.Email,
-                UserName = moderatorUserVM.UserName,
-            };
+                AppUser moderatorUser = new()
+                {
+                    FirstName = moderatorUserVM.FirstName,
+                    LastName = moderatorUserVM.LastName,
+                    Email = moderatorUserVM.Email,
+                    UserName = moderatorUserVM.UserName,
+                };
 
-            await _userManager.CreateAsync(moderatorUser, moderatorUserVM.Password);
+                await _userManager.CreateAsync(moderatorUser, moderatorUserVM.Password);
 
-            await _userManager.AddToRoleAsync(moderatorUser, "Moderator");
+                await _userManager.AddToRoleAsync(moderatorUser, "Moderator");
             }
             return Ok("Succesfully");
+        }
+
+        private async Task SendConfirmationEmail(AppUser user)
+        {
+            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+
+
+            //string url = @$"https://localhost:7091/account/ConfirmEmail?token={token}&userId={user.Id}";
+            string url = Url.Action("ConfirmEmail", "account", new { token = token, userId = user.Id }, Request.Scheme) ?? string.Empty;
+
+            string emailBody = $@"<h2>Email Confirmation</h2>
+                <p>Hello {user.FirstName} {user.LastName}</p>
+                <p>Please confirm your email by clicking the link below:</p>
+                <a href='{url}'>Confirm Email</a>";
+
+            await _emailService.SendEmailAsync(user.Email!, "Confirm your email", emailBody);
+        }
+        public async Task<IActionResult> ConfirmEmail(string token, string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user is null)
+            {
+                return BadRequest();
+            }
+            var result = await _userManager.ConfirmEmailAsync(user, token);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest();
+            }
+            await _signInManager.SignInAsync(user, false);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
